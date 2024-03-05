f4db1f0b8020b1ca7c0389afeb3ec27fd5ecaddd
Anastasia <apushkova@sfedu.ru>
2024-03-05 17:26:06 +0300

    update lab3

--- 3/lab3/lab3/main.cpp
+++ 3/lab3/lab3/main.cpp
@@ -10,10 +10,10 @@ int main() {
 	tr.insert(15);
 	tr.insert(10);
 	tr.insert(30);
-	tr.insert(45);
+	tr.insert(-45);
 	tr.insert(12);
-	tr.insert(16);
-	tr.insert(29);
+	tr.insert(-16);
+	tr.insert(-29);
 	tr.insert(34);
 
 
@@ -32,15 +32,28 @@ int main() {
 	tr2.insert(29);
 	tr2.insert(34);
 
-	//Tree<int>::Node* n = tr.previous(tr.find(28));
+	Tree<int>::Node* n = tr.previous(tr.find(28));
 	tr.printLKP();
-	/*tr.deleteElem(10);
+	tr.deleteElem(10);
 	tr.printLKP();
 	tr.deleteElem(16);
 	tr.printLKP();
 	tr.deleteElem(25);
-	tr.printLKP();*/
+	std::cout << "Prints:\n\n";
+	tr.printLKP();
 	tr.printPKL();
-	//tr.printLevels();
-	//std::cout << (tr == tr2);
+	tr.printLevels();
+	std::cout << "\n\n";
+	std::cout << "tr.find(12): " << *(tr.find(12)) << "\n";
+	std::cout << "tr.find(122) == nullptr: " << (tr.find(122) == nullptr) << "\n";
+	std::cout << "tr.findMax(): " << *tr.findMax() << "\n";
+	std::cout << "tr.findMin(): " << *tr.findMin() << "\n";
+	std::cout << "tr.upperBound(25): " << *tr.upperBound(25) << "\n";
+	std::cout << "tr.upperBound(26): " << *tr.upperBound(26) << "\n";
+	std::cout << "tr.lowerBound(25): " << *tr.lowerBound(25) << "\n";
+	std::cout << "tr.lowerBound(26): " << *tr.lowerBound(26) << "\n";
+	std::cout << "tr.previous(tr.find(28)): " << *tr.previous(tr.find(28)) << "\n";
+	std::cout << "tr.next(tr.find(28)): " << *tr.next(tr.find(28)) << "\n";
+	std::cout << "(tr == tr2): " << (tr == tr2) << "\n";
+	std::cout << "(tr == tr): " << (tr == tr) << "\n";
 }
---
+++ 3/lab3/lab3/my_set.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "tree.h"
+
+template<typename T>
+class MySet {
+	Tree t;
+
+public:
+	
+
+	//Todo 1. Добавление/удаление элемента;
+
+	
+	//todo 2. Проверка на вхождение элемента
+};
+
+
+//Sieve of Eratosthenes set
+
+
+//Sieve of Eratosthenes vector
