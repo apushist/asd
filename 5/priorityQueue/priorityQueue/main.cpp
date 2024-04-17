d755075b93d2aee46fadfcacb427cd03b3f23dd9
apushist <114854401+apushist@users.noreply.github.com>
2024-04-17 19:55:01 +0300

    update lab5

--- 5/priorityQueue/priorityQueue/main.cpp
+++ 5/priorityQueue/priorityQueue/main.cpp
@@ -8,8 +8,28 @@ int main() {
 	v.push_back("bsdfghjkl;");
 	std::sort(v.begin(), v.end());
 	for (std::string x : v) {
-		std::cout << x << " ";
+		//std::cout << x << " ";
 	}
+	//std::cout << "\n" << v.front() << " " << v.back();
+
+	myPriorityQueue1<std::string, std::deque<std::string>, StringLengthComp> q;
+	q.push("a");
+	std::string* arr = new std::string[4]{"ydythf","ggjhgjh","juu","asdfghjkl;defghjk"};
+
+	q.pushFromArray(arr,4);
+	//q.println();
+	//std::cout << q.getMax();
 	std::deque<int> d;
 	
+
+	myPriorityQueue1<int> q1;
+	myPriorityQueue1<int> q2;
+	
+	q1.push({ 1, 12, 3, 4, 25, 6 });
+	q2.push({ 7,5,2,11,16 });
+
+	q1.println();
+
+
+
 }
--- 5/priorityQueue/priorityQueue/priorityQueue.h
+++ 5/priorityQueue/priorityQueue/priorityQueue.h
@@ -5,6 +5,16 @@
 #include <deque>
 #include <algorithm>
 #include <string>
+#include <initializer_list>
+#include <exception>
+
+
+//from longest to shortest
+struct StringLengthComp {
+	bool operator()(std::string s1, std::string s2) const{
+		return s1.length() < s2.length();
+	}
+};
 
 
 template<typename T, typename Container = std::vector<T>, typename Compare = std::less<typename Container::value_type>>
@@ -15,23 +25,65 @@ class myPriorityQueue1 {
 public:
 	myPriorityQueue1(const Container& cont, const Compare& comp):container(cont),cmp(comp){}
 	myPriorityQueue1() {
-		container = new Container();
+		container = Container();
 	}
 
 	void push(const T& value) {
 		container.push_back(value);
-		std::sort(container.begin(), container.end());
+		std::sort(container.begin(), container.end(),cmp);
 	}
-	void pop();
-	const T& top() const;
-	bool empty() const;
+
+	void push(std::initializer_list<T> il) {
+		for(T x: il)
+			container.push_back(x);
+		std::sort(container.begin(), container.end(), cmp);
+	}
+
+	bool empty() const {
+		return size() == 0;
+	}
+
 	size_t size() const {
 		return container.size();
 	}
+
 	void clear() {
 		container.clear();
 	}
 
+
+	void println() {
+		std::cout << *this << "\n";
+	}
+
+	void pushFromArray(T* arr, size_t size) {
+		for (int i = 0; i < size;i++) {
+			container.push_back(arr[i]);
+		}
+		std::sort(container.begin(), container.end(), cmp);
+	}
+
+
+	T getMax() {
+		if (empty())
+			throw std::exception("Empty queue");
+		return container.back();
+	}
+
+	void merge(const myPriorityQueue1 other) {
+		for (T x : other.container) {
+			container.push_back(x);
+		}
+		std::sort(container.begin(), container.end(), cmp);
+	}
+
+
+	friend std::ostream& operator<<(std::ostream& os, const myPriorityQueue1& q) {
+		for (T x : container) {
+			os << x << " ";
+		}
+		return os;
+	}
 };
 
 
@@ -44,5 +96,35 @@ class myPriorityQueue2 {
 
 public:
 
+	myPriorityQueue2(const Container& cont, const Compare& comp) :container(cont), cmp(comp) {}
+	myPriorityQueue2() {
+		container = new Container();
+	}
 
+	void push(const T& value) {//todo
+		//container.push_back(value);
+		//std::sort(container.begin(), container.end(),cmp);
+
+
+	}
+	bool empty() const {
+		return size() == 0;
+	}
+	size_t size() const {
+		return container.size();
+	}
+	void clear() {
+		container.clear();
+	}
+
+	void println() {
+		std::cout << *this << "\n";
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const myPriorityQueue1& q) {
+		for (T x : container) {
+			os << x << " ";
+		}
+		return os;
+	}
 };
