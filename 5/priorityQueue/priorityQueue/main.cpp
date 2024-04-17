856eed3c6a845f02e0345dd35852c0cec1430cc1
apushist <114854401+apushist@users.noreply.github.com>
2024-04-18 00:50:39 +0300

    update lab 5

--- 5/priorityQueue/priorityQueue/main.cpp
+++ 5/priorityQueue/priorityQueue/main.cpp
@@ -1,35 +1,55 @@
 #include "priorityQueue.h"
 
 int main() {
-	std::vector<std::string> v;
-	v.push_back("gdsjj");
-	v.push_back("affff");
-	v.push_back("asdfghjkl;");
-	v.push_back("bsdfghjkl;");
-	std::sort(v.begin(), v.end());
-	for (std::string x : v) {
-		//std::cout << x << " ";
-	}
-	//std::cout << "\n" << v.front() << " " << v.back();
+	//std::vector<std::string> v;
+	//v.push_back("gdsjj");
+	//v.push_back("affff");
+	//v.push_back("asdfghjkl;");
+	//v.push_back("bsdfghjkl;");
+	//std::sort(v.begin(), v.end());
+	//for (std::string x : v) {
+	//	//std::cout << x << " ";
+	//}
+	////std::cout << "\n" << v.front() << " " << v.back();
 
-	myPriorityQueue1<std::string, std::deque<std::string>, StringLengthComp> q;
-	q.push("a");
-	std::string* arr = new std::string[4]{"ydythf","ggjhgjh","juu","asdfghjkl;defghjk"};
+	//myPriorityQueue1<std::string, std::deque<std::string>, StringLengthComp> q;
+	//q.push("a");
+	//std::string* arr = new std::string[4]{"ydythf","ggjhgjh","juu","asdfghjkl;defghjk"};
 
-	q.pushFromArray(arr,4);
-	//q.println();
-	//std::cout << q.getMax();
-	std::deque<int> d;
-	
+	//q = myPriorityQueue1<std::string, std::deque<std::string>, StringLengthComp>(arr,4);
+	////q.println();
+	////std::cout << q.getMax();
+	//std::deque<int> d = {12,2,3,4};
+	//
+	////std::cout << v[0];
+	//
 
-	myPriorityQueue1<int> q1;
-	myPriorityQueue1<int> q2;
-	
-	q1.push({ 1, 12, 3, 4, 25, 6 });
-	q2.push({ 7,5,2,11,16 });
+	//myPriorityQueue2<int> q1 = myPriorityQueue2<int>(new int[6]{ 1, 12, 3, 4, 25, 6 },6);
+	//myPriorityQueue2<int> q2 = myPriorityQueue2<int>(new int[5] { 7, 5, 2, 11, 16 }, 5);
+	//
 
-	q1.println();
+	//q1.println();
+	//q2.println();
+
+	//q1.merge(q2);
+
+	//q1.println();
+	//std::cout << q1.getMax() << "\n";
+	//q1.println();
+	setlocale(LC_ALL, "Russian");
+
+	std::string filename = "lotr.txt";
+
+	myPriorityQueue1<std::string, std::deque<std::string>, StringLengthComp> q1;
+	myPriorityQueue2<std::string, std::deque<std::string>, StringLengthComp> q2;
 
 
+	getWordsFromFile(filename, q1);
+	std::cout << "ÐÅÀËÈÇÀÖÈß 1:\n";
+	print20Max(q1);
+
+	getWordsFromFile(filename, q2);
+	std::cout << "\n\nÐÅÀËÈÇÀÖÈß 2:\n";	
+	print20Max(q2);
 
 }
--- 5/priorityQueue/priorityQueue/priorityQueue.h
+++ 5/priorityQueue/priorityQueue/priorityQueue.h
@@ -7,7 +7,10 @@
 #include <string>
 #include <initializer_list>
 #include <exception>
+#include <fstream>
 
+
+int SIZE = 2000;
 
 //from longest to shortest
 struct StringLengthComp {
@@ -15,6 +18,36 @@ struct StringLengthComp {
 		return s1.length() < s2.length();
 	}
 };
+
+template<typename Container>
+void print20Max(Container queue) {
+	for (int i = 0; i < 20;i++) {
+		std::cout << queue.getMax() << " ";
+	}
+	std::cout << "\n";
+}
+
+template<typename Container>
+void getWordsFromFile(const std::string& filename, Container& queue) {
+	std::ifstream file(filename);
+	if (!file.is_open()) {
+		std::cerr << "Unable to open file " << filename << std::endl;
+		return;
+	}
+
+	std::string word;
+	int i = 0;
+	while (file >> word && i < SIZE) {
+		
+		if (!word.empty()) {
+			//std::cout << word;
+			queue.push(word);
+			i++;
+		}
+	}
+
+	file.close();
+}
 
 
 template<typename T, typename Container = std::vector<T>, typename Compare = std::less<typename Container::value_type>>
@@ -26,6 +59,13 @@ public:
 	myPriorityQueue1(const Container& cont, const Compare& comp):container(cont),cmp(comp){}
 	myPriorityQueue1() {
 		container = Container();
+	}
+
+	myPriorityQueue1(T* arr, size_t size) {
+		for (int i = 0; i < size;i++) {
+			container.push_back(arr[i]);
+		}
+		std::sort(container.begin(), container.end(), cmp);
 	}
 
 	void push(const T& value) {
@@ -56,18 +96,15 @@ public:
 		std::cout << *this << "\n";
 	}
 
-	void pushFromArray(T* arr, size_t size) {
-		for (int i = 0; i < size;i++) {
-			container.push_back(arr[i]);
-		}
-		std::sort(container.begin(), container.end(), cmp);
-	}
+	
 
 
 	T getMax() {
 		if (empty())
 			throw std::exception("Empty queue");
-		return container.back();
+		T res = container.back();
+		container.pop_back();
+		return res;
 	}
 
 	void merge(const myPriorityQueue1 other) {
@@ -79,11 +116,13 @@ public:
 
 
 	friend std::ostream& operator<<(std::ostream& os, const myPriorityQueue1& q) {
-		for (T x : container) {
+		for (T x : q.container) {
 			os << x << " ";
 		}
 		return os;
 	}
+
+	
 };
 
 
@@ -98,14 +137,51 @@ public:
 
 	myPriorityQueue2(const Container& cont, const Compare& comp) :container(cont), cmp(comp) {}
 	myPriorityQueue2() {
-		container = new Container();
+		container = Container();
+	}
+	myPriorityQueue2(T* arr, size_t size) {
+		for (int i = 0; i < size;i++) {
+			container.push_back(arr[i]);
+		}
+		for (int i = size / 2; i >= 0;i--) {
+			heapify(i);
+		}
+
 	}
 
-	void push(const T& value) {//todo
-		//container.push_back(value);
-		//std::sort(container.begin(), container.end(),cmp);
+private:
+
+	void heapify(unsigned int i) {
+		unsigned int left = 2 * i + 1;
+		unsigned int right = 2 * i + 2;
+		unsigned int largest = i;
+		if (left < size() && cmp(container[largest],container[left])) {
+			largest = left;
+		}
+		if (right < size() && cmp(container[largest], container[right])) {
+			largest = right;
+		}
+		if (largest != i) {
+			T temp = container[i];
+			container[i] = container[largest];
+			container[largest] = temp;
+			heapify(largest);
+		}
+	}
+
+public:
 
 
+	void push(const T& value) {
+		container.push_back(value);
+		
+		unsigned int i = size()-1;
+		while (i > 0 && cmp(container[(i-1)/2],container[i]))
+		{
+			std::swap(container[(i-1) / 2], container[i]);
+			
+			i = (i-1) / 2;
+		}
 	}
 	bool empty() const {
 		return size() == 0;
@@ -121,10 +197,28 @@ public:
 		std::cout << *this << "\n";
 	}
 
-	friend std::ostream& operator<<(std::ostream& os, const myPriorityQueue1& q) {
-		for (T x : container) {
+	friend std::ostream& operator<<(std::ostream& os, const myPriorityQueue2& q) {
+		for (T x : q.container) {
 			os << x << " ";
 		}
 		return os;
+	}
+
+	T getMax() {
+		if (empty())
+			throw std::exception("Empty queue");
+		T mx = container[0];
+		container[0] = container[size() - 1];
+		container.pop_back();
+		heapify(0);
+		return mx;
+	}
+
+	void merge(const myPriorityQueue2 other) {
+		container.insert(container.end(),other.container.begin(), other.container.end());
+
+		for (int i = (size() / 2) - 1; i >= 0; --i) {
+			heapify(i);
+		}
 	}
 };
